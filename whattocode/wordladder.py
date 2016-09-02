68a5c02954bd3dd1674d32a7db9307ce37881275
James Crovo <jamescrovo2019@lschs.org>
2016-09-02 00:33:38 -0400

    Edit wordladder; split main function in two; change tabs to spaces

--- whattocode/wordladder.py
+++ whattocode/wordladder.py
@@ -4,52 +4,53 @@
 #Given two words, wordladder converts the first word into the second one letter at a time
 #Optional: pass the argument "random" to make the order that letter are changed random
 
-import sys
+import argparse, sys
 from random import shuffle
 
-def wordLadder(random_order = False):
-	word_one = input("Phrase 1: ").lower()
-	original_one = word_one
-	word_two = input("Phrase 2: ").lower()
-	original_two = word_two
-	print("")
+def main(random_order = False):
+    word_one = input("Phrase 1: ").lower()
+    word_two = input("Phrase 2: ").lower()
+    print("")
 	
-	difference = abs(len(word_one) - len(word_two))
-	if len(word_one) > len(word_two):
-		word_two += " "*difference
-	else:
-		word_one += " "*difference
+    difference = abs(len(word_one) - len(word_two))
+    if len(word_one) > len(word_two):
+        word_two += " "*difference
+    else:
+        word_one += " "*difference
 		
-	if word_one == word_two:
-		print("Words are equal.\n")
-		return
+    if word_one == word_two:
+        print("Words are equal.\n")
+        return
 		
-	word_one = list(word_one)
-	word_two = list(word_two)
-	steps = 0
-	order = list(range(0,len(word_one)))
-	if random_order is True:
-		shuffle(order)
-	for letter in order:
-		if word_one[letter] == word_two[letter]:
-			pass
-		else:
-			steps += 1
-			step = "{}. {} -> ".format(steps, "".join(word_one))
-			word_one[letter] = word_two[letter]
-			step += "".join(word_one)
-			print(step)
-	print("\n\"{firstword}\" converted to \"{secondword}\" in {amount} steps.\n".format(firstword=original_one, secondword=original_two, amount=steps))
+    word_one = list(word_one)
+    word_two = list(word_two)
+	
+    convertWords(word_one, word_two, random_order)
+	
+def convertWords(word_list_one, word_list_two, random_order):
+    steps = 0
+    original_one = "".join(word_list_one)
+    original_two = "".join(word_list_two)
+    order = list(range(0,len(word_list_one)))
+    if random_order is True:
+        shuffle(order)
+    for letter in order:
+        if word_list_one[letter] != word_list_two[letter]:
+            steps += 1
+            step = "{}. {} -> ".format(steps, "".join(word_list_one))
+            word_list_one[letter] = word_list_two[letter]
+            step += "".join(word_list_one)
+            print(step)
+    print("\n\"{firstword}\" converted to \"{secondword}\" in {amount} steps.\n".format(firstword=original_one, secondword=original_two, amount=steps))
 
 if __name__ == "__main__":
-	try:
-		if sys.argv[1].lower() == "random":
-			wordLadder(random_order = True)
-		else:
-			wordLadder()
-	except IndexError:
-		wordLadder()
-	
+    try:
+        if sys.argv[1].lower() == "-random":
+            main(random_order = True)
+        else:
+            main()
+    except IndexError:
+	    main()
 	
 	
 	
