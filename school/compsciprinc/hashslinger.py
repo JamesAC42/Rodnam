589d175de259207eecfe98f9b94014e84c48b929
James Crovo <jamescrovo2019@lschs.org>
2016-09-14 21:53:15 -0400

    Add compsci and password hasher

---
+++ school/compsciprinc/hashslinger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import sys
+
+def hash(pw):
+    alph = 'abcdefghijklmnopqrstuvwxyz'
+    spec = '!@#$%^&*()'
+    new = []
+    pw = list(pw)
+    pw.reverse()
+    for letter in pw:
+        new.append(letter)
+        new.append(str(alph.index(letter)))
+    new.append(spec[int(new[-1])])
+    if len(new) <= 10:
+        wen = [x for x in new]
+        wen.reverse()
+        new += wen
+    return("".join(new))
+
+if __name__ == "__main__":
+	pw = sys.argv[1]
+	print(hash(pw))
+
+
