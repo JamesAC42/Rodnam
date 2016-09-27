0e27377b3713cb4ef2426c76a9f9dedab907d27d
James Crovo <jamescrovo2019@lschs.org>
2016-09-26 22:03:13 -0400

    Add cipher code

---
+++ school/compsciprinc/encrypt.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import sys
+
+#Input
+filename = sys.argv[1]
+file = open(filename, 'r')
+passage = file.read()
+new = []
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+#Encrypt/ decrypt passage
+for letter in passage:
+	if letter not in alphabet:
+		if letter is " ":
+			new.append(letter)
+		else:
+			continue
+	else:
+		if letter is 'a':
+			new.append('z')
+			continue
+		else:
+			letterspace = ((alphabet.index(letter) * 26) % 27) - 2
+			newletter = alphabet[letterspace]
+			new.append(newletter)
+	
+#Output
+file.close()
+filenew = open(filename, 'w')
+filenew.write("".join(new))
+filenew.close()
+print("".join(new))
+
+'''
+	My cipher takes a message and encrypts it using the following process. First, take the index of the letter and multiply it by 26. Then 
+'''
+	
--- school/compsciprinc/hashslinger.py
+++ school/compsciprinc/hashslinger.py
@@ -4,20 +4,24 @@ import sys
 
 def hash(pw):
     alph = 'abcdefghijklmnopqrstuvwxyz'
-    spec = '!@#$%^&*()'
-    new = []
-    pw = list(pw)
-    pw.reverse()
-    for letter in pw:
-        new.append(letter)
-        new.append(str(alph.index(letter)))
-    new.append(spec[int(new[-1])])
-    if len(new) <= 10:
+    spec = list('!@#$%^&*()')
+    new = []
+    pw = list(pw)
+    pw.reverse()
+	
+    for letter in range(0,len(pw)):
+        new.append(pw[letter])
+        new.append(int(alph.index(pw[letter])) * 11)
+    new = [str(x) for x in new]
+    iindex = round(int(new[-1]) / 10)
+    new.append(spec[iindex])
+	
+    if len(new) <= 7:
         wen = [x for x in new]
         wen.reverse()
-        new += wen
-    return("".join(new))
-
+        new += wen
+		
+    return(("".join(new)))
 if __name__ == "__main__":
 	pw = sys.argv[1]
 	print(hash(pw))
