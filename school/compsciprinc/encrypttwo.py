fa92d94b3dad45d397011d7f9c22c27e6f428339
James Crovo <jamescrovo2019@lschs.org>
2016-10-10 22:24:14 -0400

    Edit encryption

--- school/compsciprinc/encrypttwo.py
+++ school/compsciprinc/encrypttwo.py
@@ -1,42 +1,64 @@
+#!/usr/bin/python
+
+import sys
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
 def hash(pw):
-    alph = 'abcdefghijklmnopqrstuvwxyz'
-    spec = list('!@#$%^&*()')
-    new = []
-    pw = list(pw)
-    pw.reverse()
-	
-    for letter in range(0,len(pw)):
-        new.append(pw[letter])
-        new.append(int(alph.index(pw[letter])) * 11)
-    new = [str(x) for x in new]
-    iindex = round(int(new[-1]) / 10)
-    new.append(spec[iindex])
-	
-    if len(new) <= 7:
-        wen = [x for x in new]
-        wen.reverse()
-        new += wen
-		
-    return(("".join(new)))
+	pw = pw.lower()
+	spec = list('!@#$%^&*()')
+	new = []
+	pw = list(pw)
+	pw.reverse()
+	for letter in range(0,len(pw)):
+		new.append(pw[letter])
+		new.append(int((alphabet.index(pw[letter])) + 1) * 11)
+	new = [str(x) for x in new]
+	iindex = (int(new[-1]) % 10) - 1
+	new.append(spec[iindex])
+	if len(new) <= 7:
+		wen = [x for x in new]
+		wen.reverse()
+		new += wen
+	return(("".join(new)))
 
 def encrypt(pw):
+	pw = pw.lower()
 	new = []
 
-	alphabet = 'abcdefghijklmnopqrstuvwxyz'
 	numbers = list(range(10))
-	for letter in passage:
+	for letter in pw:
 		if letter not in alphabet:
-			if letter is " ":
-				new.append(letter)
-			else:
-				new.append(letter)
+			new.append(letter)
 		else:
-			if letter is 'a':
+			'''if letter is 'a':
 				new.append('z')
 				continue
-			else:
-				letterspace = ((alphabet.index(letter) * 26) % 27) - 2
-				newletter = alphabet[letterspace]
-				new.append(newletter)
+			else:'''
+			letterspace = (((alphabet.index(letter) + 1) * 26) % 27) - 1
+			newletter = alphabet[letterspace]
+			new.append(newletter)
+			
 	return("".join(new))
+	
+def complicate(pw):
+	pw = pw.lower()
+	numbers = [str(x) for x in range(10)]
+	new = []
+	for character in pw:
+		if character in numbers:
+			new.append(alphabet[int(character) - 1])
+			new.append(character)
+		else:
+			new.append(character)
+	return("".join(new))
+			
+	
+if __name__ == "__main__":
+	weak_pw = sys.argv[1]
+	#final  = complicate(encrypt(hash(weak_pw)))
+	hashed_pw = hash(weak_pw)
+	encrypted_pw = encrypt(hashed_pw)
+	final_pw = complicate(encrypted_pw)
+	print(final_pw)
 	
