83b0c034e2a08b57d55828b35f095a88e195f8d9
James Crovo <jamescrovo2019@lschs.org>
2017-07-08 22:30:20 -0400

    Add custom timer functionality

--- projects/workday/timer.js
+++ projects/workday/timer.js
@@ -1,7 +1,7 @@
 var date = new Date();
-var currentTime = ((date.getHours() * 60 * 60) + (date.getMinutes() * 60) + (date.getSeconds())) - 28800;
-var endTime = 32400;
-var timeLeft = endTime - currentTime;
+var currentTime = 0;
+var endTime = 0;
+var timeLeft = 0;
 
 var numberWords = {
 	"1":"one",
@@ -16,33 +16,6 @@ var numberWords = {
 	"0":"zero"
 };
 
-setInterval(function(){
-	if((currentTime > 0) && (currentTime < endTime)){
-		let wordList = numberToDigits(timeLeft);
-		wordList.reverse();
-		let n = 0;
-		$(".number-outer-inner").each(function(){
-			$(this).removeClass();
-			$(this).addClass("number-outer-inner");
-			$(this).addClass(wordList[n]);
-			n++;
-		});
-		var percent = Math.floor((currentTime / endTime) * 100);
-		$(".progress-bar-inner").css("width", percent + "%");
-	}else{
-		if(currentTime >= endTime){
-			$(".number-outer-inner").each(function(){
-				$(this).removeClass();
-				$(this).addClass("number-outer-inner zero");
-			});
-			$(".progress-bar-inner").css("width", "100%");
-			$(".progress-bar-inner").css("animation", "flash .5s infinite");
-		}
-	}
-	currentTime++;
-	timeLeft--;
-},1000);
-
 function numberToDigits(n){
 	var s = n.toString();
 	var digitList = s.split('');
@@ -55,3 +28,64 @@ function numberToDigits(n){
 	});
 	return wordList;
 }
+
+function renderTime(){
+	if((currentTime > 0) && (currentTime < endTime)){
+		let wordList = numberToDigits(timeLeft);
+		wordList.reverse();
+		let n = 0;
+		$(".number-outer-inner").each(function(){
+			$(this).removeClass();
+			$(this).addClass("number-outer-inner");
+			$(this).addClass(wordList[n]);
+			n++;
+		});
+		var percent = Math.floor((currentTime / endTime) * 100);
+		$(".progress-bar-inner").css("width", percent + "%");
+		$(".progress-bar-inner").css("animation", "none");
+	}else{
+		if(currentTime >= endTime){
+			$(".number-outer-inner").each(function(){
+				$(this).removeClass();
+				$(this).addClass("number-outer-inner zero");
+			});
+			$(".progress-bar-inner").css("width", "100%");
+			$(".progress-bar-inner").css("animation", "flash .5s infinite");
+		}
+	}
+	currentTime++;
+	timeLeft--;
+}
+
+function setWork(){
+	currentTime = ((date.getHours() * 60 * 60) + (date.getMinutes() * 60) + (date.getSeconds())) - 28800;
+	endTime = 32400;
+	timeLeft = endTime - currentTime;
+}
+
+function setSchool(){
+	currentTime = ((date.getHours() * 60 * 60) + (date.getMinutes() * 60) + (date.getSeconds())) - 29700;
+	endTime = 25200;
+	timeLeft = endTime - currentTime;
+}
+
+function setCustom(){
+	if(/^\d+$/.test($("#seconds-input").val())){
+		currentTime = 1;
+		endTime = parseInt($("#seconds-input").val()) + 1;
+		timeLeft = endTime - currentTime;
+		$("#seconds-input").val("");
+	}else{
+		alert("Invalid Time");
+	}
+}
+
+$(".tab").click(function(){
+	$(".settings").toggleClass("visible");
+	$(".tab").toggleClass("tab-on");
+});
+$(".submit").click(setCustom);
+$(".set-work").click(setWork);
+$(".set-school").click(setSchool);
+
+setInterval(renderTime,1000);
