50be73f4b7b50a4f3c3c22b0ebf08c17776587e2
James Crovo <jamescrovo2019@lschs.org>
2017-11-28 00:32:18 -0500

    Add windows and searchbar

--- projects/startpage/js/index.js
+++ projects/startpage/js/index.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+	$(".icon").mouseover(function(){
+		$(this).prev().addClass("icon-support");
+		$(this).next().addClass("icon-support");
+	}).mouseleave(function(){
+		$(this).prev().removeClass("icon-support");
+		$(this).next().removeClass("icon-support");
+	});
 
+	$(".icons-container").mouseover(function(){
+		$(".navbar-glow").addClass("navbar-glow-highlight");
+	}).mouseleave(function(){
+		$(".navbar-glow").removeClass("navbar-glow-highlight");
+	});
 
 });
