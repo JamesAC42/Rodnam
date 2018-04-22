2c486bd90f77e985ea4725fb603a31e4aa89e103
James <jamescrovo2019@lschs.org>
2018-04-22 14:51:09 -0400

    add backend files

--- projects/music_player/parse.js
+++ projects/music_player/parse.js
@@ -1,19 +1,44 @@
 const fs = require("fs");
+const path = require("path");
 const load = require("audio-loader");
-const audioMetaData = require("audio-metadata");
+const AudioMetaData = require("audio-metadata");
 
-function song(path, metadata) {
-    this.path = path;
-    this.metadata = metadata;
-}
+var walk = function(dir, done) {
+    var results = {};
+    fs.readdir(dir, function(err, list) {
+        if (err) return done(err);
+        var pending = list.length;
+        if (!pending) return done(null, results);
+        list.forEach(function(file) {
+            file = path.resolve(dir, file);
+            fs.stat(file, function(err, stat) {
+                if (stat && stat.isDirectory()) {
+                    walk(file, function(err, res) {
+                        for(let parent in res) {
+                            if(!results[parent]) {
+                                results[parent]= [];
+                            }
+                            results[parent].push(res[parent]);
+                        }
+                        if (!--pending) done(null, results);
+                    });
+                } else {
+                    let parent = path.dirname(file).split(path.sep).pop()
+                    if(!results[parent]) {
+                        results[parent]= [];
+                    }
+                    results[parent].push(file);
+                    if (!--pending) done(null, results);
+                }
+            });
+        });
+    });
+};
 
-let contentMap = {};
-
-const parse = (dir, results) => {
-    fs.readdir(dir, (err, files) => {
-        if(err || !files.length) return;
-        for(let file in files) {
-            
-        }
-    })
-}
+walk("music/", (err, songs) => {
+    console.info(songs);
+    if (err) throw err;
+    let dataMap = require("./music_saves.json");
+    dataMap.songs = songs;
+    fs.writeFile("music_saves.json", JSON.stringify(dataMap, null, '  '), "utf8", callback=>{return});
+});
---
+++ projects/music_player/server.js
@@ -0,0 +1,73 @@
+
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+const formidable = require("formidable");
+
+const handler = (req, res) => {
+    if(req.method.toLowerCase() === 'get') {
+        displayPage(req, res);
+    } else if(req.method.toLowerCase() === 'post') {
+        let dest = req.url;
+        switch(dest) {
+            case '/getSongs':
+                getSongs(req, res);
+            default:
+                error(res, 500);
+        }
+    }
+}
+
+const server = require('http').createServer(handler);
+
+const fileTypes = {
+    '.html':'text/html',
+    '.js':'text/javascript',
+    '.css':'text/css',
+    '.mp3':'audio/mpeg',
+    '.jpg':'image/jpg',
+    '.png':'image/png',
+    '.ttf':'application/octet-stream'
+}
+
+const displayPage = (req, res) => {
+    let filePath = req.url;
+    if(filePath === '/') filePath = 'index.html';
+    filePath = __dirname+"/"+filePath;
+    let extname = path.extname(filePath);
+    let contentType;
+    if(fileTypes[extname] !== undefined) {
+        contentType = fileTypes[extname];
+    } else {
+        console.info("Invalid filetype: " + extname);
+        error(res, 500);
+    } 
+    fs.exists(filePath, function(exists) {
+        if (exists) {
+            fs.readFile(filePath, (error, content) => {
+                if (error) {
+                    error(res, 500);
+                } else {                   
+                    res.writeHead(200, { 'Content-Type': contentType });
+                    res.end(content, 'utf-8');                  
+                }
+            });
+        } else {
+            error(res, 404);
+        }
+    });
+}
+
+const getSongs = (req, res) => {
+    let dataMap = require("./music_saves.json");
+    res.writeHead(200, {"Content-Type":"text/plain"});
+    res.end(JSON.stringify(dataMap));
+}
+
+const error = (res, e) => {
+    res.writeHead(e);
+    res.end();
+}
+
+server.listen(4567);
+console.log("Listening at 4567...");
